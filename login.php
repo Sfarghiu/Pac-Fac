4a71b6d63dea2af724304dc941e18745b55e50f9
sabisav <sabisav@yahoo.com>
2017-05-21 23:37:12 +0300

    Changed buttons

--- login.php
+++ login.php
@@ -6,14 +6,14 @@
 </head>
 
 <body>
-<img id="tuplea" src="tuplea.png" >
-<img id="buraga" src="buraga.png" >
+  <img id="tuplea" src="tuplea.png" >
+  <img id="buraga" src="buraga.png" >
 
 
-<div class="container">
-  <div class="login">
-  <p id="pacfac">Pac-Fac</p>
-    <form method="post" action="http://localhost:8181/Pac-Fac/Game.php">
+  <div class="container">
+    <div class="login">
+      <p id="pacfac">Pac-Fac</p>
+      <form method="post" action="http://localhost:8181/Pac-Fac/Game.php">
       <!--<p><input type="text" name="login"  placeholder="GitHub Username or Email"></p>
       <p><input type="password" name="password" placeholder="GitHub Password"></p>
       <p class="remember">
@@ -29,10 +29,10 @@
     <p>Forgot password? <a href="#">Reset it.</a></p>
   </p>-->
 
-     <p id="signup">Don't have a GitHub account? <a href="https://github.com/join?source=login">Sign up.</a></p>
- 
-  </div>
- <img src="pac-man.jpg" alt="Pac-Man">
+  <p id="signup">Don't have a GitHub account? <a href="https://github.com/join?source=login">Sign up.</a></p>
+  
+</div>
+<img src="pac-man.jpg" alt="Pac-Man">
 
 </div>
 
